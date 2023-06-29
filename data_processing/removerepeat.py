f2f7701593eb4341c03ff7251556ba3df49a374b
Xinyue Du <130887924+LyanRy@users.noreply.github.com>
2023-06-29 03:08:32 -0400

    files used for data processing

---
+++ data_processing/removerepeat.py
@@ -0,0 +1,23 @@
+import csv
+
+def remove_duplicates(csv_file):
+    rows = []
+    seen = set()
+
+    with open(csv_file, 'r') as file:
+        reader = csv.reader(file)
+
+        for row in reader:
+            key = tuple(row)
+
+            if key not in seen:
+                rows.append(row)
+                seen.add(key)
+
+    with open(csv_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(rows)
+
+csv_file_path = 'data60.csv'
+
+remove_duplicates(csv_file_path)
